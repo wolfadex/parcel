acbe293b7a448485408193cf6ff86d8e5a6c41ee
Wolfgang Schuster <wolfgang.schuster@fourwindsinteractive.com>
2018-09-27 11:10:30 -0600

    Remove deasync

--- src/Asset.js
+++ src/Asset.js
@@ -5,7 +5,6 @@ const fs = require('./utils/fs');
 const md5 = require('./utils/md5');
 const isURL = require('./utils/is-url');
 const config = require('./utils/config');
-const syncPromise = require('./utils/syncPromise');
 const logger = require('./Logger');
 const Resolver = require('./Resolver');
 const objectHash = require('./utils/objectHash');
@@ -117,13 +116,6 @@ class Asset {
       .generateBundleName();
 
     return URL.format(parsed);
-  }
-
-  get package() {
-    logger.warn(
-      '`asset.package` is deprecated. Please use `await asset.getPackage()` instead.'
-    );
-    return syncPromise(this.getPackage());
   }
 
   async getPackage() {
